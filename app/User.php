dc3d5f6fd65919586eee29e1146e9a8fff082524
junpeko5 <junpeko5@gmail.com>
2019-11-12 05:27:01 +0900

    Setup laravel JWT authentication

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,4 +37,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        // return the primary key of the user - user id
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        // return a key value array, containing any custom claims to be added to JWT
+        return [];
+    }
 }

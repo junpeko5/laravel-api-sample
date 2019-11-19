eb73c801cc49e65f647378702a6ad7101ed96a29
junpeko5 <junpeko5@gmail.com>
2019-11-20 05:16:27 +0900

    Post Resource

--- app/Http/Resources/Topic.php
+++ app/Http/Resources/Topic.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\Post as PostResource;
 
 class Topic extends JsonResource
 {
@@ -19,6 +20,7 @@ class Topic extends JsonResource
             'title' => $this->title,
             'created_at' => $this->created_at->diffForHumans(),
             'updated_at' => $this->updated_at->diffForHumans(),
+            'post' => PostResource::collection($this->posts),
             'user' => $this->user,
         ];
     }

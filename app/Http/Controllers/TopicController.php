d233c44eef0cf2287a05e28a3d12e31e357d703a
junpeko5 <junpeko5@gmail.com>
2019-11-19 21:26:18 +0900

    Create a new topic

---
+++ app/Http/Controllers/TopicController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\Topic;
+use Illuminate\Http\Request;
+
+class TopicController extends Controller
+{
+    public function store(Request $request)
+    {
+        $topic = new Topic();
+        $topic->title = $request->title;
+        $topic->user()->associate($request->user());
+
+        $post = new Post();
+        $post->body = $request->body;
+        $post->user()->associate($request->user());
+        $topic->save();
+        $topic->posts()->save($post);
+    }
+}

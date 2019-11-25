d20496376ac1cf2a16d566cf9074a991fd6ff84e
junpeko5 <junpeko5@gmail.com>
2019-11-26 06:31:54 +0900

    Usersテーブルのseederを作成

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,8 @@
 use App\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,11 +19,14 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    $now = Carbon::now();
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => bcrypt('password'), // password
         'remember_token' => Str::random(10),
+        'created_at' => $faker->dateTimeThisYear,
+        'updated_at' => $now,
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Carbon\Carbon;
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \factory(\App\User::class, 30)->create();
+    }
+}
